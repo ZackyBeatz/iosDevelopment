2461ecb59d3fcd920bbbfa255bd89d6b9bfc6c1e
zackybeatz <zacky89@hotmail.com>
2016-01-04 13:40:10 +0100

    Twitter like app, tutorial

---
+++ PragmaticTweets/PragmaticTweets/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  PragmaticTweets
+//
+//  Created by Zakaria Ahmed on 04/01/16.
+//  Copyright © 2016 Pragmatic Programmers, LLC. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func handleTweetButtonTapped(sender: UIButton) {
+      
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter){
+            let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            tweetVC.setInitialText("I just finished the first project in iOS 8 SDK Development. #pragsios8")
+            
+            presentViewController(tweetVC, animated: true, completion: nil)
+        } else {
+            print("Can't send tweet")
+        }
+        
+    }
+    
+
+}
+
